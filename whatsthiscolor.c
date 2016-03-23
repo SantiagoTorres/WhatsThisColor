62081aa1aaf921273fa723bcd113ff795d61e721
Santiago Torres <torresariass@gmail.com>
2016-03-22 21:46:14 -0400

    ENH: simplifiex get pixel logic

--- whatsthiscolor.c
+++ whatsthiscolor.c
@@ -1,7 +1,4 @@
-#include <stdio.h>
-#include <X11/Xlib.h>
-
-
+#include "whatsthiscolor.h"
 
 /* Taken from http://rosettacode.org/wiki/Color_of_a_screen_pixel#C */
 void get_pixel_color (Display *d, int x, int y, XColor *color)
@@ -16,41 +13,31 @@ void get_pixel_color (Display *d, int x, int y, XColor *color)
 
 }
 
-
 int main(int argc, char **argv)
 {
 
     Display *display;
     XEvent xevent;
-    Window window;
+    Window window, root_return, child_return;
     int pixel;
-    XColor c;
+    XColor c, exact, screen;
+    int done = 0;
+    int rootx, rooty, winx, winy;
+    unsigned int mask;
+    int ret;
+    char *color_string;
+    Colormap colormap;
 
 
     if( (display = XOpenDisplay(NULL)) == NULL )
         return -1;
     window = DefaultRootWindow(display);
-    XAllowEvents(display, AsyncBoth, CurrentTime);
 
-    /* Get mouse pointer */
-    XGrabPointer(display, window, 1, PointerMotionMask, GrabModeAsync,
-                 GrabModeAsync, None, None, CurrentTime);
-
-    /* query the mouse position and find which color is the pixel in it */
-    while(1) {
-
-        XNextEvent(display, &xevent);
-
-        switch (xevent.type) {
-            case MotionNotify:
-                //pixel = vga_getpixel(xevent.xmotion.x_root, xevent.xmotion.y_root);
-                get_pixel_color (display, xevent.xmotion.x_root,
-                                 xevent.xmotion.y_root, &c);
-                printf("Color in cursor %04x, %04x, %04x\n", c.red, c.green,
-                                                            c.blue);
-                break;
-        }
-    }
+    /* Get mouse pointer coordinates */
+    ret = XQueryPointer(display, window, &root_return, &child_return, &rootx,
+            &rooty, &winx, &winy, &mask);
+    get_pixel_color (display, rootx, rooty, &c);
+    printf("[%x] r:%x g:%x b:%x\n", ret, c.red, c.green, c.blue);
 
     return 0;
 }
